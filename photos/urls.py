2d9848b3a3a1c2363a184ec828527f1d4a1bcc22
isaac mariga <isaacwangombe5@gmail.com>
2022-06-01 14:52:37 +0300

    Adding photos views

--- photos/urls.py
+++ photos/urls.py
@@ -4,11 +4,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 from django.urls import path
-                                                                                  
+
 
 urlpatterns=[
-  path('', views.post,name = 'post')
+  path('', views.post,name = 'post'),
+  path('photo/<id>',views.photo, name='photo' )
 ]
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
--- photos/views.py
+++ photos/views.py
@@ -3,12 +3,16 @@ from django.http  import HttpResponse
 from .models import User, Post, Comment, Location, Tag
 
 
-# Create your views here.
-def welcome(request):
-  return render(request, 'photos/photos.html')
-
-
 def post(request):
   post = Post.get_all()
   
-  return render(request,'photos/photos.html', {'post' : post})
+  return render(request,'photos/posts.html', {'post' : post})
+
+
+
+def photo(request):
+  post = Post.get_by_id(id)
+
+  return render(request, 'photos/photo.html', {'post':post, 'id':id})
+
+
