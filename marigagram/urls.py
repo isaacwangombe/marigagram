db8861d286be0e1c80e6252bc91689995e38ce79
isaac mariga <isaacwangombe5@gmail.com>
2022-06-02 15:22:56 +0300

    creating auth paths

--- marigagram/urls.py
+++ marigagram/urls.py
@@ -16,12 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.contrib.auth import views 
 
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('photos.urls'))
+    path('', include('photos.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path(r'logout/', views.LogoutView.as_view( next_page = '/')), 
+
 
 ]
