48d9f7f3044557141e5da3aee1ec5944cd85d975
isaac mariga <isaacwangombe5@gmail.com>
2022-08-25 11:15:02 +0300

    brush ups

--- marigagram/settings.py
+++ marigagram/settings.py
@@ -39,21 +39,13 @@ if config('MODE')=="dev":
        
    }
 # production
-# else:
-#    DATABASES = {
-#        'default': dj_database_url.config(
-#            default=config('DATABASE_URL')
-#        )
-#    }
 else:
- DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'marigagram3',
-         'USER': 'mariga',
-     'PASSWORD':'password',
-     }
- }
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
+
  
 
 db_from_env = dj_database_url.config(conn_max_age=500)
